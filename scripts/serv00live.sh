641d9c7ee6d70ad76d7436501e8934cf07a38836
opensh <125738740+openMJJ@users.noreply.github.com>
2024-06-30 23:42:00 +0800

    Create serv00live.sh

---
+++ scripts/serv00live.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# 定义服务器和脚本信息
+declare -A SERVERS=(
+    ["server1"]="open02@s4.serv00.com"
+    ["server2"]="open01@s4.serv00.com"
+    ["server3"]="serv0001@s4.serv00.com"
+    ["server4"]="serv0002@s4.serv00.com"
+    ["server5"]="openmjj@s4.serv00.com"
+    ["server6"]="qq56607@s0.serv00.com"
+    ["server7"]="serv0004@s4.serv00.com"
+    ["server8"]="serv501@s4.serv00.com"
+    ["server9"]="open03@s4.serv00.com"
+    # 可以在这里添加更多服务器
+    # ["server2"]="user@server.com"
+)
+
+# SSH 登录密码
+PASSWORD="Tss@19740522"
+
+# pm2_resurrect_and_restart.sh 脚本内容
+PM2_SCRIPT=$(cat <<'EOF'
+#!/bin/bash
+
+# 获取当前用户的 home 目录
+HOME_DIR=$(eval echo ~$USER)
+
+# Resurrect PM2 processes
+~/.npm-global/bin/pm2 resurrect
+
+# Check for stopped or errored processes and restart them
+~/.npm-global/bin/pm2 list | grep -E 'stopped|errored' | awk '{print $2}' | while read id; do
+    ~/.npm-global/bin/pm2 restart $id
+done
+EOF
+)
+
+# 遍历服务器并执行脚本
+for server_key in "${!SERVERS[@]}"; do
+    server="${SERVERS[$server_key]}"
+    echo "Logging into $server"
+
+    # 将 pm2_script 内容作为临时脚本传输到远程服务器
+    echo "Creating temporary script on $server"
+    sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt $server "echo \"$PM2_SCRIPT\" > ~/pm2_temp_script.sh && chmod +x ~/pm2_temp_script.sh"
+
+    # 执行临时脚本
+    echo "Executing temporary script on $server"
+    sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt $server "bash -c '~/pm2_temp_script.sh'"
+
+    # 删除临时脚本
+    echo "Deleting temporary script on $server"
+    sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt $server "rm ~/pm2_temp_script.sh"
+done
